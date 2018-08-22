629c1ba82888d8e42d490583ed7375eb94dc31d1
servilla <mark.servilla@gmail.com>
2018-08-21 20:34:14 -0600

    Add wsgi.py to be executed from uwsgi

---
+++ wsgi.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+""":Mod: wsgi.py
+
+:Synopsis:
+
+:Author:
+    servilla
+
+:Created:
+    8/21/18
+"""
+from webapp.run import app
+
+if __name__ == '__main__':
+    app.run()
