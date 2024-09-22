ff85f6d471bc4e47009a5ee315396787a90973e3
Colin Smith <colin.smith@wisc.edu>
2024-09-22 10:52:01 -0700

    Mitigate risks of overriding imported class methods
    Create local copies of required properties and pass them to
    `soso.main.convert` using `kwargs` to prevent unintended modifications
    to imported class methods. This addresses concerns raised by commit
    0631e98d3ec064a157f8bfd6cbd93b1ce45ffb3f.
    
    Related to #31

--- webapp/schema_org.py
+++ webapp/schema_org.py
@@ -14,13 +14,13 @@
 import json
 import os.path
 import tempfile
+from typing import Union
+from mimetypes import guess_type
 
 import requests
+from lxml import etree
 from soso.main import convert
-from soso.strategies.eml import (EML, get_content_size, get_content_url,
-                                 get_data_entity_encoding_format, get_checksum)
-from soso.strategies.eml import get_encoding_format
-from soso.utilities import delete_null_values, generate_citation_from_doi
+from soso.utilities import generate_citation_from_doi
 
 from webapp.utility import pid_triple
 
@@ -81,7 +81,9 @@ def dataset(pid: str, env: str = None, raw: str = None):
             json_ld = convert_eml_to_schema_org(
                 file_path=filename,
                 pid=pid,
-                doi=doi
+                doi=doi,
+                pasta=pasta,
+                portal=portal,
             )
 
         # Reformat the JSON-LD for readability and write to cache
@@ -96,109 +98,6 @@ def dataset(pid: str, env: str = None, raw: str = None):
         response = f"{open_tag}{j}{close_tag}"
 
     return response
-
-
-def convert_eml_to_schema_org(file_path: str, pid: str, doi: str, pasta: str,
-                              portal: str) -> str:
-    """Convert EML to Schema.org JSON-LD
-
-    :param file_path: Path to the data package metadata file.
-    :param pid: Data package identifier (scope.identifier.revision).
-    :param doi: Data package Digital Object Identifier. Must be URL
-        prefixed.
-    :param pasta: PASTA base URI.
-    :param portal: Data portal base URI.
-    """
-
-    # Add properties that can't be derived from the EML record
-    scope, identifier, revision = pid_triple(pid)
-    uri = (f"{portal}/mapbrowse?scope={scope}&identifier={identifier}&"
-           f"revision={revision}")
-    is_accessible_for_free = True
-    citation = generate_citation_from_doi(doi, style="apa", locale="en-US")
-    provider = {"@id": "https://edirepository.org"}
-    publisher = {"@id": "https://edirepository.org"}
-    if doi is not None:  # development and staging environments don't have DOIs
-        identifier = {  # DOI is more informative than the packageId
-            "@id": doi,
-            "@type": "PropertyValue",
-            "propertyID": "https://registry.identifiers.org/registry/doi",
-            "value": doi.split("https://doi.org/")[1],
-            "url": doi
-        }
-    else:
-        identifier = None
-
-    # Modify the get_subject_of method to add the missing contentUrl
-    def get_subject_of(self):
-        scope, identifier, revision, _ = os.path.basename(self.file).split(".")
-        pasta = self.kwargs.get("pasta")
-        encoding_format = get_encoding_format(self.metadata)
-        date_modified = self.get_date_modified()
-        if encoding_format and date_modified:
-            subject_of = {
-                "@type": "DataDownload",
-                "name": "EML metadata for dataset",
-                "description": "EML metadata describing the dataset",
-                "encodingFormat": encoding_format,
-                "contentUrl": f"{pasta}/metadata/eml/{scope}/{identifier}/"
-                              f"{revision}",
-                "dateModified": date_modified,
-            }
-            return delete_null_values(subject_of)
-        return None
-    # Override the get_subject_of method of the EML strategy
-    EML.get_subject_of = get_subject_of
-
-    # Modify the get_distribution method to set a default encodingFormat when
-    # it is missing
-    def get_distribution(self):
-        distribution = []
-        data_entities = [
-            "dataTable",
-            "spatialRaster",
-            "spatialVector",
-            "storedProcedure",
-            "view",
-            "otherEntity",
-        ]
-        for data_entity in data_entities:
-            for item in self.metadata.xpath(f".//{data_entity}"):
-                encoding_format = get_data_entity_encoding_format(item)
-                if encoding_format is None:  # Default encodingFormat when missing
-                    encoding_format = "application/octet-stream"
-                data_download = {
-                    "@type": "DataDownload",
-                    "name": item.findtext(".//entityName"),
-                    "description": item.findtext(".//entityDescription"),
-                    "contentSize": get_content_size(item),
-                    "contentUrl": get_content_url(item),
-                    "encodingFormat": encoding_format,
-                    "spdx:checksum": get_checksum(item),
-                }
-                distribution.append(data_download)
-        return delete_null_values(distribution)
-    # Override the get_distribution method of the EML strategy
-    EML.get_distribution = get_distribution
-
-    # Call the convert function with the additional properties
-    additional_properties = {
-        "url": uri,
-        "version": revision,
-        "isAccessibleForFree": is_accessible_for_free,
-        "citation": citation,
-        "provider": provider,
-        "publisher": publisher,
-        "identifier": identifier,
-        "pasta": pasta,  # PASTA base URI for the subjectOf contentUrl
-    }
-    res = convert(
-        file=file_path,
-        strategy="EML",
-        **additional_properties
-    )
-
-    return res
 
 
 def repository(raw: str = None) -> str:
@@ -257,3 +156,219 @@ def repository(raw: str = None) -> str:
         response = f"{open_tag}{j}{close_tag}"
 
     return response
+
+
+def convert_eml_to_schema_org(file_path: str, pid: str, doi: str, pasta: str,
+                              portal: str) -> str:
+    """Convert EML to Schema.org JSON-LD
+
+    :param file_path: Path to the data package metadata file.
+    :param pid: Data package identifier (scope.identifier.revision).
+    :param doi: Data package Digital Object Identifier. Must be URL
+        prefixed.
+    :param pasta: PASTA base URI.
+    :param portal: Data portal base URI.
+    :returns: JSON-LD string.
+    """
+    metadata = etree.parse(file_path)
+
+    # Create properties that can't be derived from the EML record
+    scope, identifier, revision = pid_triple(pid)
+    uri = (f"{portal}/mapbrowse?scope={scope}&identifier={identifier}&"
+           f"revision={revision}")
+    is_accessible_for_free = True
+    citation = generate_citation_from_doi(doi, style="apa", locale="en-US")
+    provider = {"@id": "https://edirepository.org"}
+    publisher = {"@id": "https://edirepository.org"}
+    identifier = get_identifier(doi)
+
+    # Create properties that are insufficiently defined by the soso package
+    subject_of = get_subject_of(
+        file_path=file_path,
+        metadata=metadata,
+        pasta=pasta
+    )
+    distribution = get_distribution(metadata=metadata)
+
+    # Call the convert function with the additional properties
+    additional_properties = {
+        "url": uri,
+        "version": revision,
+        "isAccessibleForFree": is_accessible_for_free,
+        "citation": citation,
+        "provider": provider,
+        "publisher": publisher,
+        "identifier": identifier,
+        "subjectOf": subject_of,
+        "distribution": distribution,
+        "pasta": pasta,  # PASTA base URI for the subjectOf contentUrl
+    }
+    res = convert(
+        file=file_path,
+        strategy="EML",
+        **additional_properties
+    )
+
+    return res
+
+
+def get_subject_of(file_path: str, metadata: etree.ElementTree,
+                   pasta: str) -> Union[dict, None]:
+    """
+    :param file_path: Path to the data package metadata file.
+    :param metadata: file_path as an XML tree.
+    :param pasta: PASTA base URI.
+    :returns: The subjectOf property for the JSON-LD record.
+    """
+    scope, identifier, revision, _ = os.path.basename(file_path).split(".")
+    encoding_format = get_encoding_format(metadata)
+    date_modified = get_date_modified(metadata)
+    if encoding_format and date_modified:
+        subject_of = {
+            "@type": "DataDownload",
+            "name": "EML metadata for dataset",
+            "description": "EML metadata describing the dataset",
+            "encodingFormat": encoding_format,
+            "contentUrl": f"{pasta}/metadata/eml/{scope}/{identifier}/"
+                          f"{revision}",
+            "dateModified": date_modified,
+        }
+        return subject_of
+    return None
+
+
+def get_distribution(metadata: etree.ElementTree) -> list:
+    """
+    :param metadata: The metadata object as an XML tree.
+    :returns: The distribution property for the JSON-LD record.
+    """
+    distribution = []
+    data_entities = [
+        "dataTable",
+        "spatialRaster",
+        "spatialVector",
+        "storedProcedure",
+        "view",
+        "otherEntity",
+    ]
+    for data_entity in data_entities:
+        for item in metadata.xpath(f".//{data_entity}"):
+            encoding_format = get_data_entity_encoding_format(item)
+            if encoding_format is None:  # Default encodingFormat when missing
+                encoding_format = "application/octet-stream"
+            data_download = {
+                "@type": "DataDownload",
+                "name": item.findtext(".//entityName"),
+                "description": item.findtext(".//entityDescription"),
+                "contentSize": get_content_size(item),
+                "contentUrl": get_content_url(item),
+                "encodingFormat": encoding_format,
+                "spdx:checksum": get_checksum(item),
+            }
+            distribution.append(data_download)
+    return distribution
+
+
+def get_identifier(doi: str) -> Union[dict, None]:
+    """
+    :param doi: The DOI for the data package.
+    :returns: The identifier property for the JSON-LD record.
+    """
+    if doi is not None:  # development and staging environments don't have DOIs
+        identifier = {  # DOI is more informative than the packageId
+            "@id": doi,
+            "@type": "PropertyValue",
+            "propertyID": "https://registry.identifiers.org/registry/doi",
+            "value": doi.split("https://doi.org/")[1],
+            "url": doi
+        }
+    else:
+        identifier = None
+    return identifier
+
+
+def get_content_size(data_entity_element: etree._Element) -> Union[str, None]:
+    """
+    :param data_entity_element: The data entity element to get the content
+        size from.
+    :returns: The content size of a data entity element.
+    """
+    size_element = data_entity_element.xpath(".//physical/size")
+    if size_element:
+        size = size_element[0].text
+        unit = size_element[0].get("unit")
+        if size and unit:
+            return size + " " + unit
+        return size
+    return None
+
+
+def get_content_url(data_entity_element: etree._Element) -> Union[str, None]:
+    """
+    :param data_entity_element: The data entity element to get the content
+        url from.
+    :returns: The content url for a data entity element.
+    """
+    url_element = data_entity_element.xpath(".//distribution/online/url")
+    if url_element:
+        if url_element[0].get("function") != "information":
+            return url_element[0].text
+    return None
+
+
+def get_data_entity_encoding_format(
+    data_entity_element: etree._Element,
+) -> Union[str, None]:
+    """
+    :param data_entity_element: The data entity element to get the encoding
+        format from.
+    :returns: The encoding format (as a MIME type) for a data entity element.
+    """
+    object_name = data_entity_element.findtext(".//physical/objectName")
+    if object_name:
+        encoding_format = guess_type(object_name, strict=False)[0]
+    else:
+        encoding_format = None
+    return encoding_format
+
+
+def get_checksum(data_entity_element: etree._Element) -> Union[list, None]:
+    """
+    :param data_entity_element: The data entity element to get the checksum(s)
+        from.
+    :returns: A list of dictionaries formatted as spdx:Checksum, for each
+        method attribute of the authentication element containing a
+        spdx:algorithm. Otherwise, None.
+    """
+    checksum = []
+    for item in data_entity_element.xpath(".//physical/authentication"):
+        if item.get("method") is not None and "spdx.org" in item.get("method"):
+            algorithm = item.get("method").split("#")[-1]
+            res = {
+                "@type": "spdx:Checksum",
+                "spdx:checksumValue": item.text,
+                "spdx:algorithm": {"@id": "spdx:" + algorithm},
+            }
+            checksum.append(res)
+    if len(checksum) == 0:
+        checksum = None
+    return checksum
+
+
+def get_encoding_format(metadata: etree.ElementTree) -> list:
+    """
+    :param metadata: The metadata object as an XML tree.
+    :returns: The encoding format of an EML metadata record.
+    """
+    schema_location = metadata.getroot().nsmap.get("eml", None)
+    encoding_format = ["application/xml", schema_location]
+    return encoding_format
+
+
+def get_date_modified(metadata: etree.ElementTree) -> Union[str, None]:
+    """
+    :param metadata: The metadata object as an XML tree.
+    :returns: The date modified of an EML metadata record.
+    """
+    date_modified = metadata.findtext(".//dataset/pubDate")
+    return date_modified
--- webapp/utility.py
+++ webapp/utility.py
@@ -25,5 +25,8 @@ def main():
     return 0
 
 
+
+
+
 if __name__ == "__main__":
     main()
